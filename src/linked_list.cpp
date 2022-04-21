7fd298a26fc146758da06ab72ca909171f04fad9
Dasha <dasha.ved93@gmail.com>
2022-04-21 15:21:46 +0300

    all methods done

--- src/linked_list.cpp
+++ src/linked_list.cpp
@@ -12,7 +12,17 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseIterative() {
-    // Напишите здесь свой код ...
+    Node* curr_node = front_;
+    back_ = front_;
+    Node* temporary_node = nullptr;
+    Node* prev_node = nullptr;
+    while (curr_node != nullptr){
+        temporary_node = curr_node->next;
+        curr_node->next = prev_node;
+        prev_node = curr_node;
+        curr_node = temporary_node;
+      }
+    front_ = prev_node;
   }
 
   /**
@@ -23,12 +33,24 @@ namespace assignment {
    *    по времени ~ O(N)
    */
   void LinkedList::ReverseRecursive() {
-    // Напишите здесь свой код ...
+    if (front_ != nullptr) {
+      Node* curr_node = front_;
+      Node* prev_node = nullptr;
+      reverse_recursive_helper(curr_node, prev_node);
+    }
   }
 
   // вспомогательный метод для реализации рекурсии
   void LinkedList::reverse_recursive_helper(Node*& curr, Node* prev) {
-    // Напишите здесь свой код ...
+    if (curr != back_){
+      reverse_recursive_helper(curr->next, curr);
+    }else{
+      front_ = curr;
+    }
+    if (prev == nullptr){
+      back_ = curr;
+    }
+    curr->next = prev;
   }
 
 }  // namespace assignment
